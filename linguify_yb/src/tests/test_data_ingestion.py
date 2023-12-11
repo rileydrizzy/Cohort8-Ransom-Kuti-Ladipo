b6e5e95486ae9c70adc805f100af7b64102108e6
rileydrizzy <rileydrizzy@hotmail.com>
2023-12-11 02:46:12 +0100

    [add] added  units test

---
+++ linguify_yb/src/tests/test_data_ingestion.py
@@ -0,0 +1,8 @@
+"doc"
+
+import torch
+import pytest
+
+
+def test_data_columns():
+    pass
---
+++ linguify_yb/src/tests/test_model.py
@@ -0,0 +1,39 @@
+"""doc
+"""
+
+import pytest
+
+import torch
+from torch.utils.data import DataLoader
+from src.models.baseline_transformer import ASLTransformer
+
+@pytest.fixture
+def baseline_model():
+    """_summary_"""
+    model = ASLTransformer()
+    return model
+
+
+@pytest.mark.parametrize(
+    "inputs_x, target_y", [(torch.randn(128, 345), torch.randint(0, 60, (64,)))]
+)
+def test_baseline_transformer_output_shape(baseline_model, inputs_x, target_y):
+    """_summary_"""
+    output = baseline_model(inputs_x, target_y)
+    # Assert
+    expected_output_shape = (64, 62)
+    assert output.shape == expected_output_shape
+
+@pytest.mark.parametrize("inputs_x", [(torch.randn(128,345)), (torch.randn(128,345))])
+def test_baseline_transformer_generate_out(
+    baseline_model,
+    inputs_x,
+):
+    """_summary_"""
+    output = baseline_model.generate(inputs_x)
+    # Assert
+    expected_output_len = 64
+    assert len(output) == expected_output_len
+
+@pytest.mark.parametrize("input_shape", [(batch_size, input_dim) for batch_size in [1, 2, 5] for input_dim in [32, 64, 128]])
+def
