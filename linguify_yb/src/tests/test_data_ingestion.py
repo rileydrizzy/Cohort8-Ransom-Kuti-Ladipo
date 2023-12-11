c3a78ff9241405f0630be2ccd4e6fc6c2b27be89
rileydrizzy <rileydrizzy@hotmail.com>
2023-12-11 06:09:07 +0100

    [add] added frame preprocess test

--- linguify_yb/src/tests/test_data_ingestion.py
+++ linguify_yb/src/tests/test_data_ingestion.py
@@ -1,8 +1,18 @@
 "doc"
 
-import torch
 import pytest
 
+import torch
+from torch.utils.data import DataLoader
+from src.dataset.frames_config import FRAME_LEN
+from src.dataset.preprocess import clean_frames_process
 
-def test_data_columns():
-    pass
+
+@pytest.mark.parametrize(
+    "frames",
+    [torch.randn(num_frames, 345) for num_frames in [10, 108, 128, 156, 750, 420]],
+)
+def test_frames_preprocess(frames):
+    clean_frames = clean_frames_process(frames)
+    expected_output_shape = (128, 345)
+    assert expected_output_shape == clean_frames.shape
