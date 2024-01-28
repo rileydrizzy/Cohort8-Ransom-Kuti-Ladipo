102ee3b8847308a81e79ec00f2fb7e112dd46c88
rileydrizzy <rileydrizzy@hotmail.com>
2024-01-28 10:27:08 +0100

    feat(trainer): implement code for single GPU training

--- signa2text/src/trainer.py
+++ signa2text/src/trainer.py
@@ -5,74 +5,105 @@ Classes:
 - Trainer: A class for training neural network models in a distributed setup.
 
 Functions:
-- ddp_setup: Setup Distributed Data Parallel (DDP) for training.
+
 """
 
-# TODO Complete and refactor code for distributed training
+# TODO Implement and refactor code for distributed training
+# TODO AMP Mixed Trainig and Scaled Loss
+# TODO Consider adding TPU training
 
 import os
-
-import numpy as np
+import wandb
 import torch
-from torch.utils.data import DataLoader
-from torch.nn.parallel import DistributedDataParallel as DDP
-from torch.distributed import init_process_group
-from utils.logger_util import logger
 
-
-def ddp_setup():
-    """
-    Setup Distributed Data Parallel (DDP) for training.
-    """
-    init_process_group(backend="nccl")
-    torch.cuda.set_device(int(os.environ["LOCAL_RANK"]))
+from utils.logging import logger
 
 
 class Trainer:
+    """_summary_"""
+
     def __init__(
         self,
-        model: torch.nn.Module,
-        train_data: DataLoader,
-        optimizer: torch.optim.Optimizer,
-        save_every: int,
+        model,
+        train_data,
+        optimizer,
         loss_func,
+        resume_checkpoint=False,
     ):
         """
         Initialize a Trainer instance.
 
-        Parameters:
-            - model (torch.nn.Module): The neural network model.
-            - train_data (DataLoader): The DataLoader for training data.
-            - optimizer (torch.optim.Optimizer): The optimizer for training.
-            - save_every (int): Save a snapshot of the model every `save_every` epochs.
-            - loss_func: The loss function for training.
+        Parameters
+        ----------
+        model : torch.nn.Module
+            The neural network model.
+        train_data : DataLoader
+            The DataLoader for training data.
+        optimizer : torch.optim.Optimizer
+            The optimizer for training.
+        loss_func : _type_
+            The loss function for training.
+        resume_checkpoint : bool
+            _description_
         """
-        self.gpu_id = int(os.environ["LOCAL_RANK"])
-        self.model = model.to(self.gpu_id)
+        self.gpu_device = (
+            "cuda" if torch.cuda.is_available() else "cpu"
+        )  # int(os.environ["LOCAL_RANK"])
+        self.model = model.to(self.gpu_device)
         self.train_data = train_data
         self.optimizer = optimizer
         self.loss_func = loss_func
-        self.save_every = save_every
         self.epochs_run = 0
         self.snapshot_path = "snapshot.pt"
         if os.path.exists(self.snapshot_path):
-            logger.info("Loading snapshot")
-            self._load_snapshot(self.snapshot_path)
+            self._load_snapshot(snapshot_path=self.snapshot_path)
+        elif resume_checkpoint:
+            self._load_snapshot(from_wandb=True)
 
-        self.model = DDP(self.model, device_ids=[self.gpu_id])
-
-    def _load_snapshot(self, snapshot_path):
+    def _load_snapshot(self, snapshot_path=None, from_wandb=False):
         """
         Load a snapshot of the model.
 
-        Parameters:
-            - snapshot_path (str): Path to the snapshot file.
+        Parameters
+        ----------
+        snapshot_path : str, optional
+            Path to the snapshot file, by default None
+        from_wandb : bool, optional
+            _description_, by default False
         """
-        loc = f"cuda:{self.gpu_id}"
-        snapshot = torch.load(snapshot_path, map_location=loc)
-        self.model.load_state_dict(snapshot["MODEL_STATE"])
-        self.epochs_run = snapshot["EPOCHS_RUN"]
+        logger.info("Loading snapshot")
+        if from_wandb:
+            # TODO load from wandb
+            self.model = None
+            self.epochs_run = None
+        else:
+            loc = "cuda:0"
+            snapshot = torch.load(snapshot_path, map_location=loc)
+            self.model.load_state_dict(snapshot["MODEL_STATE"])
+            self.epochs_run = snapshot["EPOCHS_RUN"]
         logger.info(f"Resuming training from snapshot at Epoch {self.epochs_run}")
+
+    def _save_snapshot(self, epoch, save_to_wandb=False):
+        """
+        Save a snapshot of the model.
+
+        Parameters
+        ----------
+        epoch : int
+            The current epoch.
+        save_to_wandb : bool, optional
+            _description_, by default False
+        """
+
+        snapshot = {
+            "MODEL_STATE": self.model.module.state_dict(),
+            "EPOCHS_RUN": epoch,
+        }
+        torch.save(snapshot, self.snapshot_path)
+        if save_to_wandb:
+            # TODO save on wandb
+            pass
+        logger.info(f"Epoch {epoch} | Training snapshot saved at {self.snapshot_path}")
 
     def _run_batch(self, source, targets):
         """
@@ -95,38 +126,40 @@ class Trainer:
         Parameters:
             - epoch (int): The current epoch.
         """
-        b_sz = len(next(iter(self.train_data))[0])
+        batch_size = len(next(iter(self.train_data))[0])
         logger.info(
-            f"[GPU{self.gpu_id}] Epoch {epoch} | Batchsize: {b_sz} | Steps: {len(self.train_data)}"
+            f"[GPU{self.gpu_device}] Epoch {epoch} | Batchsize: \
+                {batch_size} | Steps: {len(self.train_data)}"
         )
-        self.train_data.sampler.set_epoch(epoch)
+        # self.train_data.sampler.set_epoch(epoch)
         for source, targets in self.train_data:
-            source = source.to(self.gpu_id)
-            targets = targets.to(self.gpu_id)
+            source = source.to(self.gpu_device)
+            targets = targets.to(self.gpu_device)
             self._run_batch(source, targets)
 
-    def _save_snapshot(self, epoch):
-        """
-        Save a snapshot of the model.
-
-        Parameters:
-            - epoch (int): The current epoch.
-        """
-        snapshot = {
-            "MODEL_STATE": self.model.module.state_dict(),
-            "EPOCHS_RUN": epoch,
-        }
-        torch.save(snapshot, self.snapshot_path)
-        logger.info(f"Epoch {epoch} | Training snapshot saved at {self.snapshot_path}")
-
-    def train(self, max_epochs: int):
+    def train(
+        self,
+        max_epochs: int,
+        save_every,
+        wandb_monitor=False,
+    ):
         """
         Train the model for a specified number of epochs.
 
-        Parameters:
-            - max_epochs (int): The maximum number of epochs to train.
+        Parameters
+        ----------
+        max_epochs : int
+             The maximum number of epochs to train.
+        save_every : int
+            Save a snapshot of the model every `save_every` epochs.
+        wandb_monitor : bool, optional
+            _description_, by default False
         """
+
+        if wandb_monitor:
+            wandb.init(project="NSL_2_AUDIO")
+            wandb.watch(self.model)
         for epoch in range(self.epochs_run, max_epochs):
             self._run_epoch(epoch)
-            if self.gpu_id == 0 and epoch % self.save_every == 0:
-                self._save_snapshot(epoch)
+            if epoch % save_every == 0:
+                self._save_snapshot(epoch, save_to_wandb=wandb_monitor)
