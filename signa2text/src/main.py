0f59dead667d020256893789283c92363886aec5
rileydrizzy <rileydrizzy@hotmail.com>
2024-01-28 10:34:12 +0100

    refactor(main): rewrite code for single GPU training

--- signa2text/src/main.py
+++ signa2text/src/main.py
@@ -9,42 +9,58 @@ Module for distributed training with PyTorch using Distributed Data Parallel (DD
 # TODO add wandb for monitoring and saving model state
 
 import torch
+import hydra
 
-
+from omegaconf import DictConfig
 from utils.tools import parse_args, set_seed
 from utils.logging import logger
 from models.model_loader import ModelLoader
-from dataset.dataset_loader import get_dataset, prepare_dataloader, get_test_dataset
+from dataset.dataset_loader import get_dataset, prepare_dataloader  # get_test_dataset
 from dataset.dataset_paths import get_dataset_paths
-from trainer import Trainer, ddp_setup
-from torch.distributed import destroy_process_group
+
+from trainer import Trainer
+
+# from torch.distributed import destroy_process_group
 
 
-def load_train_objs(model_name, files_paths):
+def load_train_objs(model_name):
     """
     Load training objects, including the model, optimizer, dataset, and criterion.
 
-    Parameters:
-        - model_name (str): Name of the model to be loaded.
-        - files: Optional parameter for specifying files.
+    Parameters
+    ----------
+    model_name : str
+        Name of the model to be loaded.
+    files_paths :
+        Optional parameter for specifying files.
 
-    Returns:
-        - model: The loaded model.
-        - optimizer_: The optimizer for training.
-        - dataset: The training dataset.
-        - criterion: The loss criterion for training.
+    Returns
+    -------
+    _type_
+        model: The loaded model optimizer_: The optimizer for training.
+        dataset: The training dataset criterion: The loss criterion for training.
     """
     model = ModelLoader().get_model(model_name)
 
     # Optimizes given model/function using TorchDynamo and specified backend
     torch.compile(model)
+
     optimizer_ = torch.optim.SGD(model.parameters(), lr=0.01, momentum=0.9)
     criterion = torch.nn.CrossEntropyLoss(label_smoothing=0.1)
-    dataset = get_test_dataset()  # get_dataset(files_paths)
-    return model, optimizer_, criterion, dataset
+
+    return (
+        model,
+        optimizer_,
+        criterion,
+    )
 
 
-def main(model_name: str, save_every: int, total_epochs: int, batch_size: int):
+def main(
+    model_name: str,
+    save_every: int,
+    total_epochs: int,
+    batch_size,
+):
     """
     Main function for training a model.
 
@@ -59,31 +75,34 @@ def main(model_name: str, save_every: int, total_epochs: int, batch_size: int):
         f"Batch Size -> {batch_size}, model to be saved every -> {save_every} epoch"
     )
 
-    # To ensure reproducibility of the training process
-    set_seed()
-
     try:
-        # train, valid = get_dataset_paths()
-        ddp_setup()
-        model, optimizer, criterion, dataset = load_train_objs(
-            model_name, files_paths=None
-        )
-        
-        #! DEBUG and add validation process
+        # To ensure reproducibility of the training process
+        set_seed()
+
+        train_paths, valid_paths = get_dataset_paths(dev_mode=True)
+
+        model, optimizer, criterion = load_train_objs(model_name)
+
+        train_dataset = get_dataset(train_paths)  # get_test_dataset()
         train_dataset = prepare_dataloader(
-            dataset,
+            train_dataset,
             batch_size,
         )
+        valid_dataset = get_dataset(valid_paths)  # get_test_dataset()
+        valid_dataset = prepare_dataloader(
+            valid_dataset,
+            batch_size,
+        )
+
         trainer = Trainer(
             model=model,
             train_data=train_dataset,
             optimizer=optimizer,
-            save_every=save_every,
             loss_func=criterion,
+            resume_checkpoint=False,
         )
 
-        trainer.train(total_epochs)
-        destroy_process_group()
+        trainer.train(total_epochs, save_every=save_every, wandb_monitor=False)
 
         logger.success(f"Training completed: {total_epochs} epochs .")
 
@@ -92,11 +111,11 @@ def main(model_name: str, save_every: int, total_epochs: int, batch_size: int):
 
 
 if __name__ == "__main__":
-    arg = parse_args()
-    logger.info(f"Model to be trained is: {arg.model_name}")
+    arg_ = parse_args()
+    logger.info(f"Model to be trained is: {arg_.model_name}")
     main(
-        model_name=arg.model_name,
-        save_every=arg.save_every,
-        total_epochs=arg.epochs,
-        batch_size=arg.batch,
+        model_name=arg_.model_name,
+        save_every=arg_.save_every,
+        total_epochs=arg_.epochs,
+        batch_size=arg_.batch,
     )
