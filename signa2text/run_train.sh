a30a1d455c702fda0e7d810b36d01984a40f4eb3
Ipadeola Ladipo Ezekiel <rileydrizzy@hotmail.com>
2024-03-16 06:39:31 +0000

    mics

--- signa2text/run_train.sh
+++ signa2text/run_train.sh
@@ -1,5 +1,8 @@
 #!/bin/bash
 
+echo "Setting Enviroment Variables"
+. ./set_environment_variables.sh
+
 # Display a header with script information
 echo "=== Running Train Script ==="
 python src/main.py
